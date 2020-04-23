d435c1b1255ad033b483516b2be61ef737f866aa
iYaroslav <samardak.yaroslav@gmail.com>
2020-04-23 08:51:02 +0500

    Updates

---
+++ precache-manifest.2cfd6c732c5965df6487e828244ee0e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a864c4bf38f7bbdb33686cd2a6e42985",
+    "url": "/youtube-settings-button/index.html"
+  },
+  {
+    "revision": "d112f6dc46d7c42fe5ea",
+    "url": "/youtube-settings-button/static/css/main.bc89d211.chunk.css"
+  },
+  {
+    "revision": "c0b5c96b257e1e788592",
+    "url": "/youtube-settings-button/static/js/2.7871bc5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-settings-button/static/js/2.7871bc5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d112f6dc46d7c42fe5ea",
+    "url": "/youtube-settings-button/static/js/main.b529c0b7.chunk.js"
+  },
+  {
+    "revision": "eb1d74cb6362bd175cd5",
+    "url": "/youtube-settings-button/static/js/runtime-main.34049ef0.js"
+  }
+]);
